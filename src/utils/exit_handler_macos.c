f08463e22b0ea0147d0fbed6a7ae21a160c17c2b
rpdjf <ruipaulo.unify@outlook.fr>
2024-01-18 14:13:58 +0100

    Fix macos leak issues (why using macos ?)

--- src/utils/exit_handler_macos.c
+++ src/utils/exit_handler_macos.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exit_handler.c                                     :+:      :+:    :+:   */
+/*   exit_handler_macos.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rude-jes <ruipaulo.unify@outlook.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 00:52:28 by rude-jes          #+#    #+#             */
-/*   Updated: 2024/01/18 13:31:40 by rude-jes         ###   ########.fr       */
+/*   Updated: 2024/01/18 14:13:38 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	secure_exit(void *param)
 	cleargarbage();
 	if (param)
 		so_long = (t_so_long *)param;
-	if (param)
+	if (so_long->win)
 		mlx_destroy_window(so_long->mlx, so_long->win);
 	exit(0);
 	return (0);
@@ -33,7 +33,7 @@ int	error_exit(void *param)
 	cleargarbage();
 	if (param)
 		so_long = (t_so_long *)param;
-	if (param)
+	if (so_long->win)
 		mlx_destroy_window(so_long->mlx, so_long->win);
 	exit(1);
 	return (1);
@@ -47,7 +47,7 @@ int	crash_exit(void *param)
 	cleargarbage();
 	if (param)
 		so_long = (t_so_long *)param;
-	if (param)
+	if (so_long->win)
 		mlx_destroy_window(so_long->mlx, so_long->win);
 	exit(1);
 	return (1);
